5b760f581aa3e7408127a33b293e70efc961da3e
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-04 23:46:42 +0900

    :sparkles: feat: todoItem List 컴포넌트 생성 아이템 목록들을 출력할 컴포넌트 생성 후 App과 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import TodoItemInputField from "./components/TodoItemInputField";
+import TodoItemList from "./components/TodoItemList";
 
 function App() {
 
   return (
     <div className="App">
       <TodoItemInputField onCreate={() => {}}/>
+      <TodoItemList todoItemList={[]}/>
     </div>
   )
 }
---
+++ src/components/TodoItemList.jsx
@@ -0,0 +1,14 @@
+const TodoItemList = (props) => {
+
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return <li key={index}>{todoItem.todoItemContent}</li>;
+  });
+
+  return (
+    <div>
+      <ul>{todoList}</ul>
+    </div>
+  )
+}
+
+export default TodoItemList;
