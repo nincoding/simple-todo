feba6f9560d266246e56b75396ed1b00a6355e22
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-05 02:32:19 +0900

    :sparkles: feat: 글로벌 공통 스타일링 추가 글로벌 스타일 컴포넌트 생성 후 공통 스타일링 속성 설정

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+  }
+  body {
+    font-family: 'CookieRun';
+    background-color: #f6f6f6;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+  #root {
+    background-color: white;
+    box-shadow: rgba(100,100,111,0.2) 0px 7px 29px 0px;
+  }
+  .App {
+    min-height: 100vh;
+    @media screen and (min-width: 650px) {
+      width: 640px;
+    }
+    @media screen and (max-width: 650px) {
+      width: 90vw;
+    }
+  }
+`;
+
+export default GlobalStyle;
